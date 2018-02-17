d9a485028b69487e4ca5cd649a28d449ef94132a
deanturpin <git@lvbtn.uk>
2018-02-17 13:18:57 +0000

    Simplify headings

--- create_index.sh
+++ create_index.sh
@@ -20,12 +20,12 @@ what you can"t afford to lose. Prices fetched periodically from <a
 href="https://www.cryptocompare.com/api/" target="blah">CryptoCompare</a>. See
 the source on <a href="$git" target="blah">GitHub</a>.</p>
 
-<h2>Current positions</h2>
+<h2>Open</h2>
 <pre>
 $(cat buys.csv)
 </pre>
 
-<h2>Complete trades</h2>
+<h2>Closed</h2>
 <pre>
 $(cat sells.csv)
 </pre>
