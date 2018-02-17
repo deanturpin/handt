13f33ff79432a22ca6f8128f21293b1ab40670a2
deanturpin <git@lvbtn.uk>
2018-02-17 10:16:55 +0000

    Fetch values for a single coin and print

---
+++ exchange.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import json
+import requests
+
+coin = "ETH"
+url = ("https://min-api.cryptocompare.com/data/histohour?fsym="
+    + coin + "&tsym=USD&limit=168&aggregate=1&e=CCCAGG")
+
+prices = requests.get(url).json();
+
+if prices:
+    if prices["Response"] == "Error":
+        print("0 0 0 0")
+    else:
+        p = []
+        for spot in prices["Data"]:
+
+            pivot = (float(spot["low"])
+                + float(spot["close"])
+                + float(spot["high"])) / 3
+
+            p.append(pivot)
+
+        print(coin, end=" ")
+        for val in p:
+            print(val, end=" ")
+        print()
+
