9a7b20a5508feef78d33b4f998925b395a292bb9
deanturpin <git@lvbtn.uk>
2018-02-17 19:10:28 +0000

    Add notes field

--- position.h
+++ position.h
@@ -18,19 +18,20 @@ struct position {
   double yield = 0.0;
   str duration = "duration";
   str exchange = "cryptocompare";
+  str notes = "notes";
 
   // Streaming out
   friend std::ostream &operator<<(std::ostream &os, const position &p) {
     return os << std::fixed << p.buy_time << " " << p.sell_time << " " << p.name
-              << "\t" << p.yield << "\t" << p.duration << " " << p.buy_price
-              << " " << p.sell_price << " " << p.exchange << " " << p.strategy
-              << "\n";
+      << "\t" << p.yield << "\t" << p.duration << " " << p.buy_price << " " <<
+      p.sell_price << " " << p.exchange << " " << p.notes << " " << p.strategy
+      << "\n";
   }
 
   // Streaming in
   friend std::istream &operator>>(std::istream &is, position &p) {
     return is >> p.buy_time >> p.sell_time >> p.name >> p.yield >> p.duration >>
-           p.buy_price >> p.sell_price >> p.exchange >> p.strategy;
+      p.buy_price >> p.sell_price >> p.exchange >> p.notes >> p.strategy;
   }
 };
 
