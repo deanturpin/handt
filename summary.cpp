0a7ee689a5c44d20684af2cd2517c16191012cc1
deanturpin <git@lvbtn.uk>
2018-02-20 07:35:01 +0000

    Print total buys and sells

--- summary.cpp
+++ summary.cpp
@@ -1,34 +1,50 @@
+#include <map>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 #include "position.h"
 
-int main() {
+auto get_positions(const std::string file) {
 
   std::vector<trade_position> positions;
 
-  // Get closed positions
-  {
-    std::ifstream in("sells.csv");
-    if (in.good()) {
-      trade_position p;
-      while (in >> p)
-        positions.push_back(p);
-    }
+  std::ifstream in(file);
+  if (in.good()) {
+    trade_position p;
+    while (in >> p)
+      positions.push_back(p);
   }
 
-  std::cout << positions.size() << " positions\n";
+  return positions;
+}
+
+int main() {
+
+  // Get closed positions
+  {
+    const auto positions = get_positions("buys.csv");
+    std::cout << positions.size() << " buys\n";
+
+    // Sum the sell prices
+    std::map<std::string, double> coins;
+    double in, out;
+    for (const auto &p : positions) {
+      coins[p.name] += p.sell_price;
+
+      out += p.yield;
+      in += 100;
+    }
+
+    // std::cout << "gross return\n";
+    // for (const auto &c : coins)
+      // std::cout << c.first << "\t" << c.second << "\n";
+  }
+
 
   // Get open positions
   {
-    std::ifstream in("buys.csv");
-    if (in.good()) {
-      trade_position p;
-      while (in >> p)
-        positions.push_back(p);
-    }
+    const auto positions = get_positions("sells.csv");
+    std::cout << positions.size() << " sells\n";
   }
-
-  std::cout << positions.size() << " positions\n";
 }
