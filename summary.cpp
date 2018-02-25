09cb06090401f40a90116ec6e025185c7beaa812
deanturpin <git@lvbtn.uk>
2018-02-25 12:35:06 +0000

    Reword

--- summary.cpp
+++ summary.cpp
@@ -16,7 +16,7 @@ int main() {
   // Get the coins
   const auto prices = get_prices();
   std::cout << strategies.size() << " strategies\n";
-  std::cout << prices.size() << " coins updated per minute\n";
+  std::cout << prices.size() << " coins updated in the last minute\n";
 
   // Get the positions
   const std::vector<trade_position> positions(get_positions("positions.csv"));
