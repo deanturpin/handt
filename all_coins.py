4e800ad1a0e6e4dcd3bec7bef1ffd3bd5654fc75
deanturpin <git@lvbtn.uk>
2018-02-25 10:54:01 +0000

    Don't parse JSON if request failed - closes #18

--- all_coins.py
+++ all_coins.py
@@ -3,10 +3,14 @@
 import json
 import requests
 
-url = "https://min-api.cryptocompare.com/data/all/coinlist"
-r = requests.get(url)
-prices = r.json();
+try:
+    url = "https://min-api.cryptocompare.com/data/all/coinlist"
+    r = requests.get(url)
+    prices = r.json();
 
-for coin in prices["Data"]:
-    if prices["Data"][coin]["IsTrading"] == True:
-        print(coin)
+    for coin in prices["Data"]:
+        if prices["Data"][coin]["IsTrading"] == True:
+            print(coin)
+
+except Exception as e:
+    do = "nothing"
