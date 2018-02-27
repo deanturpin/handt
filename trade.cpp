190cd516771b45fd5a3efc6d5771a9e1fbc9f8ad
deanturpin <git@lvbtn.uk>
2018-02-27 13:06:15 +0000

    Add an extra threshold

--- trade.cpp
+++ trade.cpp
@@ -1,5 +1,5 @@
 #include "position.h"
-#include "strategy2.h"
+#include "strategy.h"
 #include "utils.h"
 #include <fstream>
 #include <iostream>
@@ -78,42 +78,42 @@ int main() {
     if (series.back() < 1.0)
       continue;
 
-    // TODO - transform strat lib into list of buys
-
     // Assess viability of a trade for each strategy
     for (const auto &buy : strategies) {
 
       // If the strategy returns positively then check if we already hold a
-      // position
-      const auto s = buy(series, 1.2);
-      const auto strategy = s.first;
-      const bool exec = s.second;
+      // position, repeat for multiple thresholds
+      for (const auto &threshold : {1.05, 1.1, 1.2, 1.3, 1.4}) {
 
-      if (exec) {
+        const auto decision = buy(series, threshold);
+        const std::string strategy = decision.first;
+        const bool execute = decision.second;
 
-        // Try to find a matching existing position
-        const auto it = std::find_if(positions.cbegin(), positions.cend(),
-                                [&name, &strategy](const auto &p) {
-                                return p.name == name &&
-                                p.strategy == strategy;
-                                });
+        if (execute) {
 
-        if (it == positions.cend()) {
-          trade_position pos;
-          pos.name = name;
+          // Try to find a matching existing position
+          const auto it = std::find_if(positions.cbegin(), positions.cend(),
+                                       [&name, &strategy](const auto &p) {
+                                       return p.name == name &&
+                                       p.strategy == strategy;
+                                       });
 
-          // Initialise buy and sell to same price
-          pos.buy_price = pos.sell_price = spot;
+          // If there isn't one create a position with current price
+          if (it == positions.cend()) {
 
-          pos.strategy = strategy;
-          pos.yield = 100.0 * pos.sell_price / pos.buy_price;
+            trade_position pos;
+            pos.name = name;
+            pos.buy_price = pos.sell_price = spot;
+            pos.strategy = strategy;
+            pos.yield = 100.0 * pos.sell_price / pos.buy_price;
 
-          // Initialise timestamp, sell price updated each time it is reviewed
-          pos.timestamp = timestamp();
-          pos.duration = 1;
-          pos.open = true;
+            // Initialise timestamp, sell price updated each time it is reviewed
+            pos.timestamp = timestamp();
+            pos.duration = 1;
+            pos.open = true;
 
-          new_positions.push_back(pos);
+            new_positions.push_back(pos);
+          }
         }
       }
     }
@@ -122,6 +122,7 @@ int main() {
   std::cout << positions.size() << " existing position\n";
   std::cout << new_positions.size() << " new position\n";
 
+  // Append new positions to existing
   std::copy(new_positions.cbegin(), new_positions.cend(), std::back_inserter(positions));
 
   // Trading session is complete, sort all positions prior to storing
