defb4d7f623a4d4e8dd268545f27b6357dc3c91b
deanturpin <git@lvbtn.uk>
2018-02-27 20:12:00 +0000

    Create list of threshold strats and ratio strats

--- strategy.h
+++ strategy.h
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <numeric>
 #include <sstream>
+#include <algorithm>
 #include <vector>
 
 namespace lft {
@@ -52,9 +53,51 @@ result spiking(series s, threshold t) {
 // |xxxx    |
 // |xxxxxxxx|
 // |xxxxxxxx|
-result stepping(series s, threshold t) {
-  const auto name = NAME("stepping", t);
+result stepping_up(series s, threshold t) {
+  const auto name = NAME("stepping_up", t);
   const bool exec = TRAILING(s) / LEADING(s) > t;
+  return result(name, exec);
+}
+
+result stepping_down(series s, threshold t) {
+  const auto name = NAME("stepping_down", t);
+  const bool exec = LEADING(s) / TRAILING(s) > t;
+  return result(name, exec);
+}
+
+result rolling_average(series s, threshold t) {
+  const auto name = NAME("roll_average", t);
+  const unsigned long length = 10;
+
+  // Start from the beginning
+  const auto start = s.cbegin();
+
+  // End a filter length from the end
+  const auto end = std::prev(s.cend(), length);
+
+  vector<double> raverage;
+  std::transform(start, end, std::back_inserter(raverage),
+                 [&length](const auto &i) {
+                 const auto start = &i;
+                 const auto end = std::next(&i, length);
+                 return std::accumulate(start, end, 0.0) / length;
+                 });
+
+  const bool exec = SPOT(s) / raverage.back() > t;
+  return result(name, exec);
+}
+
+result average_compare(series s, threshold t) {
+
+  const auto name = NAME("average_comp", t);
+
+  const unsigned long ratio = s.size() / t;
+  const double recent_average =
+    std::accumulate(s.crbegin(), std::next(s.crbegin(), ratio), 0.0,
+                    [](auto &sum, auto &i) { return sum + i; }) /
+    ratio;
+
+  const bool exec = recent_average > AVERAGE(s);
   return result(name, exec);
 }
 
@@ -62,9 +105,29 @@ result stepping(series s, threshold t) {
 // |xxxx   x|
 // |xxxxxxxx|
 // |xxxxxxxx|
-result jumping(series s, threshold &t) {
+result jumping(series s, threshold t) {
   const auto name = NAME("jumping", t);
-  const bool exec = stepping(s, t).second && spiking(s, t).second;
+  const bool exec = stepping_down(s, t).second && spiking(s, t).second;
+  return result(name, exec);
+}
+
+// |      xx|
+// |    xxxx|
+// |  xxxxxx|
+// |xxxxxxxx|
+result steady_rise(series s, threshold t) {
+  const auto name = NAME("steady_rise", t);
+  double trend = 0.0;
+  for (auto i = s.cbegin(); i != std::prev(s.cend()); ++i)
+    trend += (*i < *std::next(i) ? 1.0 : -1.0);
+  const bool exec = trend > s.size() * .75;
+  return result(name, exec);
+}
+
+result kosovich(series s, threshold t) {
+  const auto name = NAME("kosovich", t);
+  const double high = *std::max_element(s.cbegin(), s.cend());
+  const bool exec = SPOT(s) / high > t;
   return result(name, exec);
 }
 
@@ -77,12 +140,13 @@ double AVERAGE(series s) {
              : 0.0;
 }
 
+// TODO - rename to RECENT
 double LEADING(series s) {
 
   // Find the mid point of the series
   const unsigned long mid_point = s.size() / 2;
   return mid_point > 0.0
-             ? accumulate(s.crbegin(), next(s.crend(), mid_point), 0.0,
+             ? accumulate(s.crbegin(), next(s.crbegin(), mid_point), 0.0,
                           [](auto &sum, const auto &i) { return sum + i; }) /
                    mid_point
              : 0.0;
@@ -93,27 +157,42 @@ double TRAILING(series s) {
   // Find the mid point of the series
   const unsigned long mid_point = s.size() / 2;
   return mid_point > 0.0
-             ? accumulate(s.cbegin(), next(s.cend(), mid_point), 0.0,
+             ? accumulate(s.cbegin(), next(s.cbegin(), mid_point), 0.0,
                           [](auto &sum, const auto &i) { return sum + i; }) /
                    mid_point
              : 0.0;
 }
 
-string NAME(const string n, threshold t) { return n + "-" + to_string(t); }
+string NAME(const string n, threshold t) { return to_string(t) + "-" + n; }
 
 double SPOT(series s) { return s.back(); }
 
 vector<string> run_strategies(series s) {
 
+  // Strats that take thresholds
   const vector<double> thresholds {1.05, 1.1, 1.2, 1.3, 1.4};
+  const vector<std::function<result(series, threshold)>> strategy_library1{
+    crashing, spiking, jumping, stepping_up, stepping_down, steady_rise,
+    kosovich, rolling_average
+  };
 
-  // Strategy library
-  const vector<std::function<result(series, threshold)>> strategy_library{
-    crashing, spiking, jumping, stepping};
+  // Strats that take ratios
+  const vector<double> ratios {2, 3, 4};
+  const vector<std::function<result(series, threshold)>> strategy_library2{
+    average_compare
+  };
 
   vector<string> trades;
-  for (const auto &buy : strategy_library)
+  for (const auto &buy : strategy_library1)
     for (const auto &t : thresholds) {
+      const auto b = buy(s, t);
+
+      if (b.second)
+        trades.push_back(b.first);
+    }
+
+  for (const auto &buy : strategy_library2)
+    for (const auto &t : ratios) {
       const auto b = buy(s, t);
 
       if (b.second)
