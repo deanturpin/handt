3c830330998e30fef6e143d70b2c37c1f515a271
deanturpin <git@lvbtn.uk>
2018-02-27 06:59:41 +0000

    Add dipper and riser

--- test.cpp
+++ test.cpp
@@ -3,10 +3,14 @@
 #include <numeric>
 #include <iomanip>
 
+#include <iostream>
 namespace lft {
 
-// AVERAGE
-std::string spot_average2(const std::vector<double> s, const double &t) {
+using result = std::pair<std::string, bool>;
+using series = const std::vector<double>;
+using threshold = const double;
+
+result dipper(series &s, threshold &t) {
     const double average =
         std::accumulate(s.cbegin(), s.cend(), 0.0,
                         [](auto &sum, const auto &i) { return sum + i; }) /
@@ -14,39 +18,63 @@ std::string spot_average2(const std::vector<double> s, const double &t) {
 
     const double spot = s.back();
 
-    return average / spot > t ? "spot_average" + std::to_string(t) : "";
+    // std::cout << average << "\n";
+    // std::cout << average / spot << "\n";
+
+    return result(
+      "dipper" + std::to_string(t),
+      average / spot > t
+      );
+}
+
+result riser(series &s, threshold &t) {
+    const double average =
+        std::accumulate(s.cbegin(), s.cend(), 0.0,
+                        [](auto &sum, const auto &i) { return sum + i; }) /
+        s.size();
+
+    const double spot = s.back();
+
+    // std::cout << average << "\n";
+    // std::cout << spot / average << "\n";
+
+    return result(
+      "riser" + std::to_string(t),
+      spot / average > t
+      );
 }
 
 // SKI JUMP
-std::string ski_jump2(const std::vector<double> s, const double &t) {
-    const unsigned long mid = s.size() / 2;
-    const double back =
-        std::accumulate(s.begin(), std::next(s.begin(), mid), 0.0,
-                        [](auto &s, auto &i) { return s + i; }) / mid;
-
-    const double front =
-        std::accumulate(s.rbegin(), std::next(s.rbegin(), mid), 0.0,
-                        [](auto &s, auto &i) { return s + i; }) / mid;
-
-    const auto spot = s.back();
-    return back / front > t && spot / front > 1.05 ?
-      "ski_jump" + std::to_string(t) : "";
-}
-
+// std::string ski_jump(const std::vector<double> s, const double &t) {
+//     const unsigned long mid = s.size() / 2;
+//     const double back =
+//         std::accumulate(s.begin(), std::next(s.begin(), mid), 0.0,
+//                         [](auto &s, auto &i) { return s + i; }) / mid;
+// 
+//     const double front =
+//         std::accumulate(s.rbegin(), std::next(s.rbegin(), mid), 0.0,
+//                         [](auto &s, auto &i) { return s + i; }) / mid;
+// 
+//     const auto spot = s.back();
+//     return back / front > t && spot / front > 1.05 ?
+//       "ski_jump" + std::to_string(t) : "";
+// }
+// 
 // ALWAYS
-std::string always_buy(const std::vector<double> s, const double &t) {
-  return "always_buy";
+result always_buy(series &s, threshold &t) {
+  return result("always_buy", true);
 }
 
 // Strategy library
 const std::vector<
 std::function<
-std::string(std::vector<double>, double)>
+std::pair<std::string, bool>(std::vector<double>, double)>
 > strategy_library{
 
-  spot_average2,
-    ski_jump2,
-    always_buy,
+  dipper,
+  riser,
+    // ski_jump,
+    // always_buy,
 };
 
 }
@@ -66,8 +94,8 @@ void dump_series(const std::vector<double> s) {
 int main() {
 
   // Create a series of prices
-  std::vector<double> series(10);
-  std::iota(series.begin(), series.end(), 100);
+  std::vector<double> series(20);
+  std::iota(series.begin(), series.end(), 50);
 
   // Thresholds
   const std::vector<double> thresholds {1.05, 1.1, 1.2, 1.3};
@@ -75,23 +103,27 @@ int main() {
   // Ascending
   dump_series(series);
   for (const auto &buy : lft::strategy_library)
-    for (const auto &t : thresholds)
-      std::cout << std::quoted(buy(series, t)) << "\n";
+    for (const auto &t : thresholds) {
+      const auto r = buy(series, t);
+      std::cout << r.first << "\t" << std::boolalpha << r.second << "\n";
+    }
 
   // Descending
   std::reverse(series.begin(), series.end());
   dump_series(series);
   for (const auto &buy : lft::strategy_library)
-    for (const auto &t : thresholds)
-      std::cout << std::quoted(buy(series, t)) << "\n";
+    for (const auto &t : thresholds) {
+      const auto r = buy(series, t);
+      std::cout << r.first << "\t" << std::boolalpha << r.second << "\n";
+    }
 
-  // Spot is peak
-  std::fill(series.begin(), series.end(), 1.0);
-  series.back() = 1000.0;
+  // // Spot is peak
+  // std::fill(series.begin(), series.end(), 1.0);
+  // series.back() = 1000.0;
 
-  dump_series(series);
-  for (const auto &buy : lft::strategy_library)
-    for (const auto &t : thresholds)
-      std::cout << std::quoted(buy(series, t)) << "\n";
+  // dump_series(series);
+  // for (const auto &buy : lft::strategy_library)
+  //   for (const auto &t : thresholds)
+  //     std::cout << std::quoted(buy(series, t)) << "\n";
 
 }
