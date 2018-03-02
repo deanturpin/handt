9b9e48d7d59c2b2ff2d93e02f202a3d0d1d5fd43
deanturpin <git@lvbtn.uk>
2018-03-02 14:14:53 +0000

    Set field width

--- summary.cpp
+++ summary.cpp
@@ -1,7 +1,4 @@
-#include "position.h"
-#include "strategy.h"
 #include "utils.h"
-#include <fstream>
 #include <iostream>
 #include <map>
 #include <numeric>
@@ -10,31 +7,39 @@
 
 int main() {
 
+  // Configure debug
+  std::stringstream out;
+  out.precision(10);
+  out << std::boolalpha;
+  out << std::fixed;
+
   // Get the coins
   const auto prices = get_prices();
-  std::cout << prices.size() << " coins updated in the last minute\n";
+  out << prices.size() << " coins updated in the last minute\n";
 
   // Get the positions
   const auto positions = get_positions("consolidate.csv");
-  std::cout << positions.size() << " positions\n\n";
+  out << positions.size() << " positions\n\n";
 
-  std::map<std::string, std::vector<double>> yield;
+  std::map<std::string, std::vector<double>> strategies;
 
   // Close all positions
   for (const auto &position : positions)
-    yield[position.strategy].push_back(position.sell_price /
+    strategies[position.strategy].push_back(position.sell_price /
                                        position.buy_price);
 
   // Individual strategy performance
-  std::cout << "STRATEGY\t\tPOS\t% RETURN\n\n";
-  for (const auto &i : yield) {
+  out << "STRATEGY\t\t POS\t% RETURN\n\n";
+  for (const auto &i : strategies) {
     const std::string strategy = i.first;
-    const unsigned long positions = i.second.size();
+    const unsigned long count = i.second.size();
     const double yield =
         100.0 * std::accumulate(i.second.cbegin(), i.second.cend(), 0.0) /
-        positions;
+        count;
 
-    std::cout << strategy << "\t" << std::fixed << positions << "\t" << yield
+    out << strategy << "\t" << std::setw(4) << count << "\t" << yield
               << "\n";
   }
+
+  std::cout << out.str();
 }
