89a5b0496362e700a9d8309e15c0adbe55f43fdf
deanturpin <git@lvbtn.uk>
2018-04-03 08:55:13 +0100

    Add review auto test

--- create_index.sh
+++ create_index.sh
@@ -12,5 +12,6 @@ $(git log -n 5 --oneline)
 </pre>
 
 <h2>Autotest</h2>
-$(diff test/refresh.csv test/refresh2.csv && echo Pass || echo Fail)
+$(diff test/refresh.csv test/refresh2.csv && echo "refresh\tpass" || echo "refresh\tfail")
+$(diff test/review.csv test/review2.csv && echo "review\tpass" || echo "review\tfail")
 !
