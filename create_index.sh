23c863bd51b04d76a0aef044feb72bd49c2a80c2
deanturpin <git@lvbtn.uk>
2018-04-04 19:52:41 +0100

    Reorder balance debug

--- create_index.sh
+++ create_index.sh
@@ -5,9 +5,9 @@ cat <<!
 <h2>Balance</h2>
 <pre>
 $(cat balance.csv) USD
-(Nominal initial balance of 1,000,000 USD and 1,000 USD trades)
-
 Positions closed this period: $(grep false review.csv | wc -l)
+
+(Nominal initial balance of 1,000,000 USD and 1,000 USD trades)
 </pre>
 
 <h2>Change log</h2>
