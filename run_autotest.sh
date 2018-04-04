cc16f683b419d6516ddd0562b97af9e6b9de88cd
deanturpin <git@lvbtn.uk>
2018-04-04 06:49:32 +0100

    Add purge autotest

--- run_autotest.sh
+++ run_autotest.sh
@@ -8,6 +8,8 @@ diff refresh.csv refresh2.csv >& /dev/null && \
   echo -e $'refresh\tpass' || echo $'refresh\tfail'
 diff review.csv review2.csv >& /dev/null && \
   echo $'review\tpass' || echo $'review\tfail'
+diff purge.csv purge2.csv >& /dev/null && \
+  echo $'purge\tpass' || echo $'purge\tfail'
 diff prospects1.csv prospects2.csv >& /dev/null && \
   echo $'prospec\tpass' || echo $'prospec\tfail'
 diff consolidate.csv consolidate2.csv >& /dev/null && \
