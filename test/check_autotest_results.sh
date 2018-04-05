3d00b812d4863cce50616c62c223a12844e0e63e
deanturpin <pair@brightcoin.uk>
2018-04-05 01:38:46 -0400

    Refresh autotest results

--- test/check_autotest_results.sh
+++ test/check_autotest_results.sh
@@ -4,17 +4,11 @@ TZ=HANDT-1 date
 
 pushd test >& /dev/null
 
-diff refresh.csv refresh2.csv >& /dev/null && \
-  echo -e $'refresh\tpass' || echo $'refresh\tfail'
-diff review.csv review2.csv >& /dev/null && \
-  echo $'review\tpass' || echo $'review\tfail'
-diff purge.csv purge2.csv >& /dev/null && \
-  echo $'purge\tpass' || echo $'purge\tfail'
-diff prospects1.csv prospects2.csv >& /dev/null && \
-  echo $'prospec\tpass' || echo $'prospec\tfail'
-diff consolidate.csv consolidate2.csv >& /dev/null && \
-  echo $'consol\tpass' || echo $'consol\tfail'
-diff balance1.csv balance2.csv >& /dev/null && \
-  echo -e $'balance\tpass' || echo $'balance\tfail'
+diff refresh.csv refresh2.csv && echo -e $'refresh\tpass' || echo $'refresh\tfail'
+diff review.csv review2.csv && echo $'review\tpass' || echo $'review\tfail'
+diff purge.csv purge2.csv && echo $'purge\tpass' || echo $'purge\tfail'
+diff prospects1.csv prospects2.csv && echo $'prospec\tpass' || echo $'prospec\tfail'
+diff consolidate.csv consolidate2.csv && echo $'consol\tpass' || echo $'consol\tfail'
+diff balance1.csv balance2.csv && echo -e $'balance\tpass' || echo $'balance\tfail'
 
 popd >& /dev/null
