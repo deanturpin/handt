b4fe4bda9adf3a5d76c0e8f2d3d485a08952319e
deanturpin <pair@brightcoin.uk>
2018-04-05 01:45:14 -0400

    Correct purge debug (open not purged)

--- purge.cpp
+++ purge.cpp
@@ -10,17 +10,17 @@ int main() {
   const auto &reviewed_positions = handt::get_reviewed_positions();
 
   // Filter closed positions
-  std::decay_t<decltype(reviewed_positions)> purged_positions;
+  std::decay_t<decltype(reviewed_positions)> open_positions;
   std::copy_if(reviewed_positions.cbegin(), reviewed_positions.cend(),
-               std::back_inserter(purged_positions),
+               std::back_inserter(open_positions),
                [](const auto &position) { return position.open; });
 
   // Print remaining positions
   std::stringstream out;
-  for (const auto &position : purged_positions)
+  for (const auto &position : open_positions)
     out << position << '\n';
 
   out << "# " << reviewed_positions.size() << " reviewed positions\n";
-  out << "# " << purged_positions.size() << " purged positions\n";
+  out << "# " << open_positions.size() << " open positions\n";
   std::cout << out.str();
 }
--- test/check_autotest_results.sh
+++ test/check_autotest_results.sh
@@ -4,11 +4,14 @@ TZ=HANDT-1 date
 
 pushd test >& /dev/null
 
+# Simple tests
 diff refresh.csv refresh2.csv && echo -e $'refresh\tpass' || echo $'refresh\tfail'
 diff review.csv review2.csv && echo $'review\tpass' || echo $'review\tfail'
 diff purge.csv purge2.csv && echo $'purge\tpass' || echo $'purge\tfail'
-diff prospects1.csv prospects2.csv && echo $'prospec\tpass' || echo $'prospec\tfail'
 diff consolidate.csv consolidate2.csv && echo $'consol\tpass' || echo $'consol\tfail'
+
+# Tests that need two results files
+diff prospects1.csv prospects2.csv && echo $'prospec\tpass' || echo $'prospec\tfail'
 diff balance1.csv balance2.csv && echo -e $'balance\tpass' || echo $'balance\tfail'
 
 popd >& /dev/null
