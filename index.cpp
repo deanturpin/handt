62713f3f6f8fd378c619f83c236110e6eb003f11
deanturpin <git@lvbtn.uk>
2018-04-23 08:27:17 +0100

    Don't link to kcov result

--- index.cpp
+++ index.cpp
@@ -74,8 +74,7 @@ what you can't afford to lose.</p>
          " or <a href='https://github.com/deanturpin/handt/issues/new' "
          "target='____'>raise an issue</a>.</p>"
          "<p>Coins processed since 16&nbsp;April&nbsp;2018: "
-      << handt::get_stats() << ", see recent <a href='kcov_results/'>code "
-                               "coverage results</a>.</p>\n\n";
+      << handt::get_stats() << ".</p>\n\n";
 
   // Structure for reporting strategy performance
   struct strategy_summary {
