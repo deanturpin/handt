94bde3dfc1bae4c249ecb23c14f15523ec54031c
deanturpin <git@lvbtn.uk>
2018-04-24 12:55:41 +0100

    Fix coverall var and add coverage rule to Travis build

--- bin/kcov.sh
+++ bin/kcov.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-make clean
-make cc=clang++ source
+# make clean
+# make cc=clang++ source
 
 results_dir=kcov_results
 flags='--exclude-path=/usr/include/c++/ --exclude-path=/usr/include/'
-coveralls="--coveralls-id=$COVERALL_ID"
+coveralls="--coveralls-id=$COVERALLS_ID"
 
 for obj in *.o; do
   kcov $coveralls $flags $results_dir ./$obj
