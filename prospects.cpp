82b3736fbf451e7875dd6ec57ecb9e1fb871dfd1
deanturpin <git@brightcoin.uk>
2018-05-22 18:52:51 +0100

    First implementation of frames

--- prospects.cpp
+++ prospects.cpp
@@ -16,21 +16,37 @@ int main() {
       // Chop the whole series up into chunks
       for (auto i = p.series.cbegin();
            i != std::prev(p.series.cend(), frame_size); ++i) {
-        decltype(p.series) frame;
 
+        decltype(p.series) frame;
         std::copy(i, std::next(i, frame_size), std::back_inserter(frame));
 
         const auto &buys = strategy::library(frame);
 
-        // If some strategies have triggered then print them
         if (!buys.empty()) {
+
+          std::cout << p.from_symbol << '-' << p.to_symbol << '\n';
+          std::cout << buys.size() << " strategies triggered\n";
+
           const auto spot = frame.back();
-          std::cout << p.from_symbol << '\t' << p.to_symbol << '\t' << spot
-                    << ' ';
-          for (const auto &buy : buys)
-            std::cout << buy << ' ';
-          std::cout << '\n';
+          std::cout << spot << ' ';
+          const auto target_price = handt::sell_threshold * spot;
+
+          // Spin forward to see if there's a sell in the future
+          if (std::find_if(i, p.series.cend(),
+                           [&target_price](const auto &price) {
+
+                             return price == target_price;
+                           }) != p.series.end())
+            std::cout << target_price << " - profit\n";
+          else
+            std::cout << target_price << " - no profit\n";
+
+          // for (const auto &buy : buys)
+          //   std::cout << buy << ' ';
+          // std::cout << '\n';
         }
+
+        break;
       }
     }
 }
