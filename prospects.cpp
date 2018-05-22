d96389c22dad8187b614065e1d5840182263cdad
deanturpin <git@brightcoin.uk>
2018-05-22 20:08:10 +0100

    Print only strategy summary for now

--- prospects.cpp
+++ prospects.cpp
@@ -15,30 +15,31 @@ int main() {
   for (const auto &p : handt::get_prices())
     if (!p.series.empty()) {
 
-      std::cout << '\n'
-                << p.from_symbol << '-' << p.to_symbol << ' ' << p.series.size()
-                << " prices\n";
+      // std::cout << '\n'
+      //           << p.from_symbol << '-' << p.to_symbol << ' ' <<
+      //           p.series.size()
+      //           << " prices\n";
 
       // Set up some key iterators
       const auto a = p.series.cbegin();
       const auto b = std::next(p.series.cbegin(), 500);
       const auto c = p.series.cend();
 
-      std::cout << std::distance(a, b) << " size of frame\n";
-      std::cout << std::distance(a, c) << " size of series\n";
+      // std::cout << std::distance(a, b) << " size of frame\n";
+      // std::cout << std::distance(a, c) << " size of series\n";
 
       // Find the max in the subsequent prices
       const auto max = *std::max_element(b, c);
       const auto spot = *std::prev(b);
       const auto target = handt::sell_threshold * spot;
 
-      std::cout << spot << " spot price\n";
-      std::cout << target << " target price\n";
-      std::cout << max << " max price\n";
+      // std::cout << spot << " spot price\n";
+      // std::cout << target << " target price\n";
+      // std::cout << max << " max price\n";
 
       // Run the strategy library
       const auto &buys = strategy::library(a, b);
-      std::cout << buys.size() << " orders\n";
+      // std::cout << buys.size() << " orders\n";
       for (const auto &b : buys)
         if (max > target)
           ++successes[b];
