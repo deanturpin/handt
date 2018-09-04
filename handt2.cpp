417551741a54f5057481bf42ed3d2a8f5d805e85
deanturpin <kali@brightcoin.uk>
2018-09-04 08:40:10 +0100

    Break pair read into separate routine

--- handt2.cpp
+++ handt2.cpp
@@ -2,20 +2,32 @@
 #include <iostream>
 #include <vector>
 
-int main() {
+// A currency pair is a duo of symbols and an exchange whence the price came
+struct currency_pair {
+  std::string from_sym{};
+  std::string to_sym{};
+  std::string exchange{};
+};
 
-  std::ifstream in("pairs.csv");
+std::vector<currency_pair> get_pairs() {
 
-  struct currency_pair {
-    std::string from_sym{};
-    std::string to_sym{};
-    std::string exchange{};
-  } p;
+  currency_pair p;
 
+  // Extract and store the portfolio of currency pairs
   std::vector<currency_pair> pairs;
-
+  std::ifstream in("pairs.csv");
   while (in >> p.from_sym >> p.to_sym >> p.exchange)
     pairs.emplace_back(p);
 
+  return pairs;
+}
+
+int main() {
+
+  const auto &pairs = get_pairs();
   std::cout << pairs.size() << " pairs read\n";
+
+  for (const auto &request : get_pairs())
+    std::cout << request.from_sym << '\t' << request.to_sym << '\t'
+              << request.exchange << '\n';
 }
