aad896aeb7265a32615a31d9f2b49462367e67b3
deanturpin <kali@brightcoin.uk>
2018-09-05 22:24:25 +0100

    Implement back test for single coin

--- handt2.cpp
+++ handt2.cpp
@@ -38,37 +38,69 @@ int main() {
     // Get the trade params
     std::ifstream in(file.path());
     if (currency_pair p; in >> p.from_sym >> p.to_sym >> p.exchange)
-      std::cout << p.from_sym << " processing\n";
+      std::cout << "Processing " << p.from_sym << " from symbol\n";
 
     // Get the prices
     const std::vector<double> prices{std::istream_iterator<double>(in), {}};
 
-    std::cout << prices.size() << " prices read\n";
+    if (!prices.empty()) {
 
-    // Backtest
-    std::cout << std::accumulate(prices.cbegin(), prices.cend(), 0.0) /
-                     prices.size()
-              << " average price\n";
+      std::cout << prices.size() << " prices read\n";
 
-    // Configure trading period
-    const unsigned int test_period = 200;
-    const unsigned int look_ahead  = test_period * 2;
+      // Backtest
+      std::cout << std::accumulate(prices.cbegin(), prices.cend(), 0.0) /
+                       prices.size()
+                << " average price\n";
 
-    // Set up some indices into the prices
-    auto begin = prices.cbegin();
-    auto end   = std::next(begin, test_period);
-    auto until = std::next(end, look_ahead);
+      // Configure trading period
+      const unsigned int analysis_window = 24;
+      const unsigned int sell_window     = analysis_window * 2;
 
-    while (until < prices.cend()) {
-      std::cout << std::distance(prices.cbegin(), begin) << '\t'
-                << (*begin > *end && *std::max_element(end, until) > *end * 1.05
-                        ? "buy"
-                        : "sell")
-                << '\n';
+      // Set up some indices into the prices
+      auto earliest_price = prices.cbegin();
+      auto latest_price   = std::next(earliest_price, analysis_window);
+      auto future_price   = std::next(latest_price, sell_window);
 
-      std::advance(begin, test_period);
-      end   = std::next(begin, test_period);
-      until = std::next(end, look_ahead);
+      // Stat reporting
+      unsigned int successes              = 0;
+      unsigned int fails                  = 0;
+      unsigned int opportunities_to_trade = 0;
+
+      while (future_price < prices.cend()) {
+
+        const double buy_threshold  = 1.2;
+        const double sell_threshold = 1.02;
+
+        // Check if we're ready to trade
+        if (*earliest_price / *latest_price > buy_threshold) {
+
+          // Find the first opportunity to sell in the future sell window
+          const auto max_price = std::max_element(latest_price, future_price);
+
+          // Look ahead to see if we would have cashed in the trade
+          if (*max_price / *latest_price > sell_threshold) {
+            ++successes;
+
+            // Move the trade window to the first opportunity to sell
+            earliest_price = max_price;
+          }
+
+        } else
+          ++fails;
+
+        std::advance(earliest_price, 1);
+
+        latest_price = std::next(earliest_price, analysis_window);
+        future_price = std::next(latest_price, sell_window);
+
+        ++opportunities_to_trade;
+      }
+
+      std::cout << successes << " successes\n";
+      std::cout << fails << " fails\n";
+      std::cout << opportunities_to_trade << " opportunities to trade\n";
+
+      std::cout << 100.0 * successes / fails << " % success rate\n";
     }
   }
 }
