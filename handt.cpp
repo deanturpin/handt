f79d5a73e975b936ca6d3299f557ee1457646f95
deanturpin <kali@brightcoin.uk>
2018-09-15 21:18:03 +0100

    Add more thresholds

--- handt.cpp
+++ handt.cpp
@@ -259,7 +259,9 @@ int main() {
 
       // Run strategies over the prices using different buy thresholds
       for (const auto &[name, buy] : handt::strategies)
-        for (const double &buy_threshold : {1.05, 1.10, 1.15, 1.20, 1.30}) {
+        for (const double &buy_threshold :
+             {1.01, 1.02, 1.03, 1.04, 1.05, 1.06, 1.07, 1.08, 1.09, 1.10, 1.125,
+              1.15, 1.20, 1.30}) {
 
           // Create a new strategy summary, initialised with basic trade info
           strategy_summary &strategy = summary.emplace_back(strategy_summary{
