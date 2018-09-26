c9a779e7c8da3548530c1ec0de7c12fa28ec011c
deanturpin <kali@brightcoin.uk>
2018-09-26 21:05:37 +0100

    Move reporting to own file

--- handt.cpp
+++ handt.cpp
@@ -1,7 +1,7 @@
 #include "handt.h"
 #include "prices.h"
+#include "report.h"
 
-#include <iostream>
 #include <list>
 #include <type_traits>
 
@@ -11,7 +11,7 @@ auto have_a_nice_day_trader() {
   handt::unit_test();
 
   // Create a set of thresholds to use with each buy strategy
-  std::vector<int> thresholds(25);
+  std::vector<int> thresholds(10);
   std::iota(thresholds.begin(), thresholds.end(), 2);
 
   // A complete strategy consists of a primary and secondary strategy and a buy
@@ -171,93 +171,10 @@ auto have_a_nice_day_trader() {
                (b.bad_deals ? b.bad_deals : .9);
   });
 
-  // Calculate total tests performed during backtesting
-  [[maybe_unused]] const auto tests_performed = std::accumulate(
-      performance.cbegin(), performance.cend(), 0ul,
-      [](unsigned int sum, const auto &p) { return sum + p.opportunities; });
-
-  // Pretty print large numbers
-  std::cout.imbue(std::locale(""));
-
-  // Strategy and trade overview
-  // std::cout << "* " << currency_pairs.size() << " currency pairs\n"
-  //           << "* " << permutations.size() << " strategies\n"
-  //           << "* " << performance.size() << " strategy/pair combinations\n"
-  //           << "* " << tests_performed << " backtests\n\n";
-
-  // Report individual strategy performance
-  // std::cout << "# Current prospects (" << sell_threshold
-  //           << " % return)\n"
-  //              "Prospects based on prices from the last 24 hours.\n\n"
-  //              "Strategy|Pair|Good/Bad|Spot\n"
-  //              "---|---|---|---\n";
-
   return performance;
 }
 
 int main() {
-
   const auto &performance = have_a_nice_day_trader();
-
-  unsigned int buy_count = 0;
-  for (const auto &s : performance)
-    if (s.buy) {
-      // Trim any trailing asterisk from symbol name
-      std::string_view from_symbol_trimmed = s.from_symbol;
-      if (from_symbol_trimmed.back() == '*')
-        from_symbol_trimmed.remove_suffix(1);
-
-      // Construct exchange URL
-      const std::string url =
-          "https://" +
-          (s.exchange == std::string("Coinbase")
-               ? "coinbase.com"
-               : s.exchange == std::string("Binance")
-                     ? "binance.com/en/trade/" +
-                           std::string(from_symbol_trimmed) + '_' + s.to_symbol
-                     : "no_url");
-
-      // Report strategy summary
-      std::cout << s.name << '|' << "[" << s.from_symbol << '-' << s.to_symbol
-                << "](" << url << ")|" << s.good_deals << '/' << s.bad_deals
-                << '|' << s.spot << '\n';
-
-      ++buy_count;
-
-      if (buy_count > 10)
-        break;
-    }
-
-  // Report individual strategy performance
-  std::cout << R"(
-# Top performers
-The results are ordered by success which is measured using the proportion of
-good to bad trades.
-
-Strategy|Pair|Good/Bad|Spot
----|---|---|---
-)";
-
-  for (const auto &s : performance) {
-
-    // Trim any trailing asterisk from symbol name
-    std::string_view from_symbol_trimmed = s.from_symbol;
-    if (from_symbol_trimmed.back() == '*')
-      from_symbol_trimmed.remove_suffix(1);
-
-    // Construct exchange URL
-    const std::string url =
-        "https://" +
-        (s.exchange == std::string("Coinbase")
-             ? "coinbase.com"
-             : s.exchange == std::string("Binance")
-                   ? "binance.com/en/trade/" +
-                         std::string(from_symbol_trimmed) + '_' + s.to_symbol
-                   : "no_url");
-
-    // Report strategy summary
-    std::cout << s.name << '|' << "[" << s.from_symbol << '-' << s.to_symbol
-              << "](" << url << ")|" << s.good_deals << '/' << s.bad_deals
-              << '|' << s.spot << '\n';
-  }
+  report_performance(performance);
 }
