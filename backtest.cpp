3816ea50b26f1f92cfb6ca236ebe319fd07060cc
deanturpin <kali@brightcoin.uk>
2018-10-02 14:53:10 +0100

    Copy everything into strategy (using refs)

--- backtest.cpp
+++ backtest.cpp
@@ -5,16 +5,19 @@
 #include <numeric>
 #include <vector>
 
+#include <iostream>
+
 // Take a container of prices and run all strategy permutations over them
 
 std::vector<backtest_t>
-run_backtests(const std::vector<trade_t> &prices,
+run_backtests(const std::vector<trade_t> &trades,
               const std::vector<strategy_t> &permutations) {
 
   // The sell strategy returns positively if the expected yield is acheived
   // within the trading window
   const double sell_threshold = 6;
-  const auto sell = [&sell_threshold](const auto &current, const auto &future) {
+  [[maybe_unused]] const auto sell = [&sell_threshold](const auto &current,
+                                                       const auto &future) {
     return std::find_if(
         current, future,
         [spot = *current, threshold = (100.0 + sell_threshold) /
@@ -23,92 +26,109 @@ run_backtests(const std::vector<trade_t> &prices,
         });
   };
 
+  // TODO - populate all backtests
+
   // Backtest each currency pair
   std::vector<backtest_t> backtests;
-  for (const auto &[from_symbol, to_symbol, exchange, latest] : prices) {
+  for (const auto &[from_symbol, to_symbol, exchange, prices] : trades) {
 
-    // Execute all strategy permutation
-    for (const auto &strat : permutations) {
+    // Construct all strategy permutations
+    for (const auto &strategy : permutations) {
 
       // Create a new strategy and add it to the summary for later
-      const auto spot = latest.back();
-      auto &backtest = backtests.emplace_back(
-          backtest_t{strat.name, from_symbol, to_symbol, exchange, spot});
-
-      // Configure trading periods for backtest
-      const unsigned int analysis_window = 24;
-      const unsigned int sell_window = analysis_window * 2;
-
-      // Set up some indices into the prices. Historic price is the first
-      // price in the analysis window and the future price is the furthest
-      // price after the current price that we're prepared to trade. e.g.,
-      // 24-hour analysis window and 48-hour trade window
-
-      // |-- analysis window --|
-      // H--------------------NOW-----------------F
-      //                       |-- trade window --|
-
-      // Initialise the price markers to the start of historic price data
-      auto historic_price_index = latest.cbegin();
-      auto current_price_index =
-          std::next(historic_price_index, analysis_window);
-      auto future_price_index = std::next(current_price_index, sell_window);
-
-      // Move windows along until we run out of prices
-      while (future_price_index < latest.cend()) {
-
-        // Test strategy
-        if (strat.execute(historic_price_index, current_price_index)) {
-
-          // Strategy triggered, so look ahead to see if it succeeded in
-          // the defined trade window
-          if (const auto sell_price_index =
-                  sell(current_price_index, future_price_index);
-              sell_price_index != future_price_index) {
-
-            const int buy_index =
-                std::distance(latest.cbegin(), current_price_index);
-            const int sell_index =
-                std::distance(latest.cbegin(), sell_price_index);
-
-            backtest.good_deals.push_back({buy_index, sell_index});
-
-            // Move the analysis window so the next iteration starts at
-            // the last sell price
-            historic_price_index = sell_price_index;
-          } else
-            backtest.bad_deals.push_back({0, 0});
-        }
-
-        // Nudge the analysis window along and update upstream prices
-        std::advance(historic_price_index, 1);
-        current_price_index = std::next(historic_price_index, analysis_window);
-        future_price_index = std::next(current_price_index, sell_window);
-
-        // Record that this was an opportunity to trade
-        ++backtest.opportunities;
-      }
-
-      // Calculate prospects using the most recent prices
-      historic_price_index = std::prev(latest.cend(), analysis_window);
-      current_price_index = latest.cend();
-
-      if (strat.execute(historic_price_index, current_price_index))
-        backtest.buy = true;
+      const auto spot = prices.back();
+      backtests.emplace_back(backtest_t{strategy.name, from_symbol, to_symbol,
+                                        exchange, spot, strategy, prices});
     }
   }
 
-  stable_sort(backtests.begin(), backtests.end(),
-              [](const auto &a, const auto &b) {
-                const unsigned int agd = a.good_deals.size();
-                const unsigned int bgd = b.good_deals.size();
+  std::cerr << backtests.size() << " strategy/pair combos\n";
+  std::cerr << permutations.size() << " perms\n";
+  std::cerr << trades.size() << " currency pairs\n";
 
-                const unsigned int abd = a.bad_deals.size();
-                const unsigned int bbd = b.bad_deals.size();
+  for (auto &backtest : backtests) {
 
-                return static_cast<double>(agd ? agd : .9) / (abd ? abd : .9) >
-                       static_cast<double>(bgd ? bgd : .9) / (bbd ? bbd : .9);
-              });
+    // std::cerr << backtest.from_symbol << ' ' << backtest.to_symbol << '\n';
+
+    // Configure trading periods for backtest
+    const unsigned int analysis_window = 24;
+    const unsigned int sell_window = analysis_window * 2;
+
+    // Set up some indices into the prices. Historic price is the first
+    // price in the analysis window and the future price is the furthest
+    // price after the current price that we're prepared to trade. e.g.,
+    // 24-hour analysis window and 48-hour trade window
+
+    // |-- analysis window --|
+    // H--------------------NOW-----------------F
+    //                       |-- trade window --|
+
+    // Initialise the price markers to the start of historic price data
+    auto historic_price_index = backtest.prices.cbegin();
+    auto current_price_index = std::next(historic_price_index, analysis_window);
+    auto future_price_index = std::next(current_price_index, sell_window);
+
+    // Move windows along until we run out of prices
+    while (future_price_index < backtest.prices.cend()) {
+
+      // Test strategy
+      if (backtest.strategy.execute(historic_price_index,
+                                    current_price_index)) {
+
+        // Strategy triggered, so look ahead to see if it succeeded in
+        // the defined trade window
+        if (const auto sell_price_index =
+                sell(current_price_index, future_price_index);
+            sell_price_index != future_price_index) {
+
+          const int buy_index =
+              std::distance(backtest.prices.cbegin(), current_price_index);
+          const int sell_index =
+              std::distance(backtest.prices.cbegin(), sell_price_index);
+
+          backtest.good_deals.push_back({buy_index, sell_index});
+
+          // Move the analysis window so the next iteration starts at
+          // the last sell price
+          historic_price_index = sell_price_index;
+        } else
+          backtest.bad_deals.push_back({0, 0});
+      }
+
+      // Nudge the analysis window along and update upstream prices
+      std::advance(historic_price_index, 1);
+      current_price_index = std::next(historic_price_index, analysis_window);
+      future_price_index = std::next(current_price_index, sell_window);
+
+      // Record that this was an opportunity to trade
+      ++backtest.opportunities;
+    }
+
+    // Calculate prospects using the most recent prices
+    historic_price_index = std::prev(backtest.prices.cend(), analysis_window);
+    current_price_index = backtest.prices.cend();
+
+    if (backtest.strategy.execute(historic_price_index, current_price_index))
+      backtest.buy = true;
+  }
+
+  std::cerr << "backtests complete\n";
+
+  // stable_sort(backtests.begin(), backtests.end(),
+  //             [](const auto &a, const auto &b) {
+  //               const unsigned int agd = a.good_deals.size();
+  //               const unsigned int bgd = b.good_deals.size();
+  //
+  //               const unsigned int abd = a.bad_deals.size();
+  //               const unsigned int bbd = b.bad_deals.size();
+  //
+  //               return static_cast<double>(agd ? agd : .9) / (abd ? abd : .9)
+  //               >
+  //                      static_cast<double>(bgd ? bgd : .9) / (bbd ? bbd :
+  //                      .9);
+  //             });
+
+  std::cerr << "sort complete\n";
 
   return backtests;
 }
--- main.cpp
+++ main.cpp
@@ -21,29 +21,29 @@ int main() {
   const auto &backtests = run_backtests(trades, strategies);
 
   // Generate report for deployment
-  const auto &prospects_report = get_report(trades, backtests, 10, true);
-  const auto &backtest_report = get_report(trades, backtests, 20, false);
+  // const auto &prospects_report = get_report(trades, backtests, 10, true);
+  // const auto &backtest_report = get_report(trades, backtests, 20, false);
 
-  // Generate detailed report of analysis
-  const auto &detailed_report = get_detailed_report(trades, backtests);
+  // // Generate detailed report of analysis
+  // const auto &detailed_report = get_detailed_report(trades, backtests);
 
   // Create printable timestamp
-  const std::time_t end_time =
-      std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-  const auto generated_timestamp = std::ctime(&end_time);
+  // const std::time_t end_time =
+  //     std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  // const auto generated_timestamp = std::ctime(&end_time);
 
-  // Calculate total backtest performed
-  const auto total_backtests = std::accumulate(
-      backtests.cbegin(), backtests.cend(), 0ul,
-      [](unsigned int sum, const auto &p) { return sum + p.opportunities; });
+  // // Calculate total backtest performed
+  // const auto total_backtests = std::accumulate(
+  //     backtests.cbegin(), backtests.cend(), 0ul,
+  //     [](unsigned int sum, const auto &p) { return sum + p.opportunities; });
 
-  std::cout.imbue(std::locale(""));
-  std::cout << "Generated " << generated_timestamp << '\n'
-            << "* " << trades.size() << " currency pairs\n"
-            << "* " << strategies.size() << " strategies\n"
-            << "* " << total_backtests << " backtests\n\n"
-            << "# Current prospects\n"
-            << prospects_report << '\n'
-            << "# 80-day backtest\n"
-            << backtest_report << '\n';
+  // std::cout.imbue(std::locale(""));
+  // std::cout << "Generated " << generated_timestamp << '\n'
+  //           << "* " << trades.size() << " currency pairs\n"
+  //           << "* " << strategies.size() << " strategies\n"
+  //           << "* " << total_backtests << " backtests\n\n"
+  //           << "# Current prospects\n"
+  //           << prospects_report << '\n'
+  //           << "# 80-day backtest\n"
+  //           << backtest_report << '\n';
 }
