9ac281e7eff7de535dc5fcedc562b42ea7130eda
deanturpin <kali@brightcoin.uk>
2018-10-08 10:58:29 +0100

    Remove debug

--- main.cpp
+++ main.cpp
@@ -15,31 +15,31 @@ int main() {
 
   unit_test();
 
-  std::cerr << "Get latest prices for all currency pairs\n";
+  // Get latest prices for all currency pairs
   const auto &trades = get_trades();
 
-  std::cerr << "Get all strategy permutations\n";
+  // Get all strategy permutations
   const auto &strategies = get_strategies();
 
-  std::cerr << "Run backtests\n";
+  // Run backtests
   const auto &backtests = run_backtests(trades, strategies);
 
-  std::cerr << "Generate reports for deployment\n";
+  // Generate reports for deployment
   const auto &prospects_report = get_report(trades, backtests, 10, true);
   const auto &backtest_report = get_report(trades, backtests, 80, false);
 
-  std::cerr << "Generate detailed report for offline analysis";
+  // Generate detailed report for offline analysis
   get_detailed_report(trades, backtests);
 
-  std::cerr << "Generate summary of all strategy performance";
+  // Generate summary of all strategy performance
   const auto &strategy_report = get_strategy_report(backtests);
 
-  std::cerr << "Create printable timestamp\n";
+  // Create printable timestamp
   using namespace std::chrono;
   const auto end_time = system_clock::to_time_t(system_clock::now());
   const auto generated_timestamp = std::ctime(&end_time);
 
-  std::cerr << "Calculate total backtests performed\n";
+  // Calculate total backtests performed
   const auto total_backtests = std::accumulate(
       backtests.cbegin(), backtests.cend(), 0ul,
       [](unsigned int sum, const auto &p) { return sum + p.opportunities; });
@@ -54,6 +54,6 @@ int main() {
             << prospects_report << '\n'
             << "# 80-day backtest\n"
             << backtest_report << '\n'
-            << "# Strategy performance summary\n"
+            << "# Overall strategy performance\n"
             << strategy_report << '\n';
 }
