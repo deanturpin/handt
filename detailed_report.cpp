a39d9486b8707d2b33dffd0c112f2bdad7c9e175
Your Name <you@example.com>
2018-11-25 16:59:46 +0000

    Standalone begin end

--- detailed_report.cpp
+++ detailed_report.cpp
@@ -12,23 +12,22 @@ void get_detailed_report(const std::vector<trade_t> &prices,
 
     // Look up the prices for this backtest
     const auto it = std::find_if(
-        prices.cbegin(), prices.cend(),
+        std::cbegin(prices), std::cend(prices),
         [from_symbol = b.from_symbol, to_symbol = b.to_symbol](const auto p) {
           return p.from_symbol == from_symbol && p.to_symbol == to_symbol;
         });
 
-    if (it != prices.cend()) {
+    if (it != std::cend(prices)) {
       const std::string file_name = "analysis/" + b.name + '-' + b.from_symbol +
                                     '-' + b.to_symbol + ".csv";
 
-      std::ofstream out(file_name);
+      std::ofstream out{file_name};
       for (const auto &p : it->prices)
         out << p << ", \n";
     }
 
     // In lieu of for_each_n :(
-    ++iterations;
-    if (iterations > 10)
+    if (++iterations > 10)
       break;
   }
 }
